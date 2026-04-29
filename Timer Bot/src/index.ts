f63085a97fce0a5af1ee326d297899e81b37bbb2
marwan-mahmoud559 <mahmoudmarwan151@gmail.com>
2026-04-29 20:49:00 +0300

    Delete Timer Bot directory

--- Timer Bot/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { startBot } from "./bot";
-import { logger } from "./lib/logger";
-
-startBot().catch((err) => {
-  logger.error({ err }, "Failed to start Discord bot");
-  process.exit(1);
-});
